fe694d3c0e0de7728af0a5e44d1efd422b7c49b4
Murilo Dutra <murilodut@gmail.com>
2023-08-03 19:31:05 -0300

    Set up docker for the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   server: {
     host: true,
     port: 4000,
@@ -5,3 +11,4 @@
       usePolling: true
     }
   }
+})
